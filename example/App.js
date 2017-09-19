c91ea2ef8ecb53bb3af87be8e6b0fafe89a4ec67
Kirill Stepkin <kirillstyopkin@gmail.com>
2017-09-19 22:43:20 +0300

    update

--- example/App.js
+++ example/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import ProgressBar from 'react-native-air-progress-bar';
 
 export default class App extends React.Component {
   render() {
@@ -8,6 +9,8 @@ export default class App extends React.Component {
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
+
+        <ProgressBar progress={25} />
       </View>
     );
   }
