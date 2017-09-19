bc38694bb4f5760952618dfd977e7bee46c78edc
Kirill Stepkin <kirillstyopkin@gmail.com>
2017-09-19 23:17:55 +0300

    update

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 import { Animated, View, StyleSheet, Dimensions } from 'react-native';
 import PropTypes from 'prop-types';
 import Icon from 'react-native-vector-icons/Ionicons';
